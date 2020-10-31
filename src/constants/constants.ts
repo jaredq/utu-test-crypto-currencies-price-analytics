9630e3f078815fb76a257e52eeb910098cafb178
Jared Qin <jaredqin@gmail.com>
2020-11-01 00:46:25 +1100

    assume a month is 30 days instead of a calendar month

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,7 @@
 export const ROW_NO_FOR_LATEST_DATA = 0;
 export const ROW_NO_FOR_ONE_DAY_AGO = 1;
 export const ROW_NO_FOR_SEVEN_DAYS_AGO = 7;
+
+// assume a month is 30 days instead of a calendar month
 export const ROW_NO_FOR_ONE_MONTH_AGO = 30;
 export const MAX_ROWS_FOR_ANALYTICS = 31;
